0bb051656e51a2ab2a3c549ba6c33a6de5e178a4
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2022-02-04 14:05:50 +0300

    changed products, added about, conditions, login, product-list 1, product-list 2 pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,47 @@
-$(function(){
+$(function () {
+
+  $(".ware__star").rateYo({
+    readOnly: true,
+    starWidth: "15px",
+    spacing: "10px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffcc00",
+  });
+
+  $('.price-filter__slider').ionRangeSlider({
+    type: "double",
+    onStart: function (data) {
+      $('.price-filter__min-price').text("$" + data.from + ".00");
+      $('.price-filter__max-price').text("$" + data.to + ".00");
+    },
+    onChange: function (data) {
+
+      $('.price-filter__min-price').text("$" + data.from + ".00");
+      $('.price-filter__max-price').text("$" + data.to + ".00");
+      
+      if(data.to > 700){
+        $('.price-filter__max-price').text("у вас столько нету :)");
+      }
+    }
+
+  });
+
   $('.top-slider__items').slick({
     dots: true,
     arrows: false,
     fade: true,
-    autoplay: true,
+    autoplay: true
   });
- 
+
   var config = {
     controls: {
       scope: 'local'
     }
   };
- 
+
   var mixer1 = mixitup('.products-week', config);
   var mixer2 = mixitup('.new-design', config);
-  
+
+
+
 });
